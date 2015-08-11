2ae3daeb1f8040c60b8059949a303408955146a3
Sathish_Krishnan <sathish_krishnan@infosys.com>
2015-08-11 23:12:32 +0530

    Add Node App

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
  
 app.get('/', function (req, res) {
-  res.send('hello indie');
+  res.send('hello indies');
 });
  
 app.listen(process.env.PORT || 5000);
