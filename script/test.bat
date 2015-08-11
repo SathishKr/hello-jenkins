8c714c0622edf5bef33b24b259926148c615e528
Sathish_Krishnan <sathish_krishnan@infosys.com>
2015-08-11 22:49:13 +0530

    Add Node App

--- script/test.bat
+++ script/test.bat
@@ -1,5 +1,5 @@
 call npm install
-call npm install -S forever
+call npm install forever -g
 call ./node_modules/.bin/mocha ./test/test.js
 call ./node_modules/.bin/forever stopall
 call ./node_modules/.bin/forever start app.js
